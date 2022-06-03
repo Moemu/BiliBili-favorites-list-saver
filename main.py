efb1acaabb1b7084b6ebb8d605cf5afb127972e4
White_mu <WhitemuTeam@outlook.com>
2022-06-03 09:04:57 +0800

    Remove Debug Code

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import PySimpleGUI as sg
 import json,os,time
 
 sg.theme('Reddit')
-os.chdir(r'E:\py\自己使用')
 
 header={'User-Agent':'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36'}
 
