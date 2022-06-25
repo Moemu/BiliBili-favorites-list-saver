88905a7fabedfb7cc7b0321ea0e68862543f225a
White_mu <WhitemuTeam@outlook.com>
2022-06-26 01:52:33 +0800

    Repair AutoUpdate.py

--- AutoSave.py
+++
@@ -1,8 +0,0 @@
-'''
-AutoUpdate(自动更新收藏夹)
-'''
-from main import update
-
-if __name__=='__main__':
-    update(Quit=True)
-    # Say Goodbye
---
+++ AutoUpdate.py
@@ -0,0 +1,25 @@
+'''
+AutoUpdate(自动更新收藏夹)
+'''
+from main import update
+from time import sleep
+import os,sys
+
+def AutoStart():
+    '''
+    自动启动设置
+    '''
+    try:
+        import win32con,win32api
+        key = win32api.RegOpenKey(win32con.HKEY_LOCAL_MACHINE,'SOFTWARE\Microsoft\Windows\CurrentVersion\Run',0, win32con.KEY_ALL_ACCESS)
+        win32api.RegSetValueEx(key,'Favlist-Saver',0,win32con.REG_SZ,sys.argv[0])
+    except:
+        print('添加至启动项失败,请检查程序管理员权限!')
+
+if __name__=='__main__':
+    if sys.argv[1] == 'AutoStart':
+        AutoStart()
+        os._exit(0)
+    while True:
+        update()
+        sleep(3600)
